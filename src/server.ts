5e0b9eb2f76a3895f79ad38bc42c3596ab316f62
mustaffxx <felipeamorimw@gmail.com>
2022-09-19 21:52:06 -0300

    added env vars

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,10 @@
-import createHttpError from 'http-errors'
+import 'dotenv/config'
 import app from './app'
 import { AppDataSource } from './data-source'
 
 AppDataSource.initialize()
     .then(() => {
-        app.listen(3000, () => console.log('listen on 3000'))
+        app.listen(parseInt(process.env.APP_PORT as string) || 3000, () => console.log(`listen on port ${process.env.APP_PORT}`))
     })
     .catch((error) => {
         console.log('data source init failed: ', error)
