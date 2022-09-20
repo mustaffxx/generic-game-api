376d8522262aa6e66aad3304b6ce162631e48385
mustaffxx <felipeamorimw@gmail.com>
2022-09-19 21:47:03 -0300

    added database initialization

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,13 @@
+import createHttpError from 'http-errors'
 import app from './app'
+import { AppDataSource } from './data-source'
 
-app.listen(3000, () => console.log('listen on 3000'))
+AppDataSource.initialize()
+    .then(() => {
+        app.listen(3000, () => console.log('listen on 3000'))
+    })
+    .catch((error) => {
+        console.log('data source init failed: ', error)
+    })
+
+
