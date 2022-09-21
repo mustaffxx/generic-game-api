e9f916d768007460034df6095f3527756632f8bb
mustaffxx <felipeamorimw@gmail.com>
2022-09-20 23:50:34 -0300

    added basic validation in UserService

--- src/services/UserService.ts
+++ src/services/UserService.ts
@@ -1,8 +1,12 @@
+import createHttpError from 'http-errors'
 import { AppDataSource } from '../data-source'
 import { User } from '../entities/User'
 
 class UserService {
     async create(username: string, role: string) {
+        if (!username || !role)
+            throw createHttpError(400, 'username or role incorrect')
+
         const userRepository = AppDataSource.getRepository(User)
 
         const user = userRepository.create({ username, role })
