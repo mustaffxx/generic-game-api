b3c099dfac9cb29c8a0582e0891417deb980a90f
mustaffxx <felipeamorimw@gmail.com>
2022-09-28 00:37:23 -0300

    added more tests to userRoute.create

--- src/__tests__/routes.spec.ts
+++ src/__tests__/routes.spec.ts
@@ -11,22 +11,39 @@ beforeEach(async () => {
 })
 
 afterAll(async () => {
-    AppDataSource.destroy()
+    await AppDataSource.destroy()
 })
 
 describe('userRoute', () => {
-    const testUsers = [
-        {
-            username: 'test1',
-            role: 'role1'
-        }, {
-            username: 'test2',
-            role: 'role2'
-        }
-    ]
-    it('should create user', async () => {
-        const response = await supertest(app).post('/api/game/user').send(testUsers[0]).set('Accept', 'application/json')
+    describe('create', () => {
+        const testUsers = [
+            {
+                username: 'test1',
+                role: 'role1'
+            }, {
+                username: 'test2',
+                role: 'role2'
+            }
+        ]
 
-        expect(response.status).toBe(201)
+        it('should create user', async () => {
+            const response = await supertest(app).post('/api/game/user').send(testUsers[0])
+
+            expect(response.status).toBe(201)
+        })
+
+        it('should return 400 status code if pass incorrect params', async () => {
+            const response = await supertest(app).post('/api/game/user').send({ username: '', role: 'role2' })
+
+            expect(response.status).toBe(400)
+        })
+
+        it('should return 409 status code if user already exists', async () => {
+            await supertest(app).post('/api/game/user').send(testUsers[0])
+
+            const response = await supertest(app).post('/api/game/user').send(testUsers[0])
+
+            expect(response.status).toBe(409)
+        })
     })
 })
