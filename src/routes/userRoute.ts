98896b1a3c42458cf5ab154a0e641c0ee5643cbb
mustaffxx <felipeamorimw@gmail.com>
2022-09-28 22:54:02 -0300

    added user get route

--- src/routes/userRoute.ts
+++ src/routes/userRoute.ts
@@ -4,5 +4,6 @@ import UserController from '../controllers/UserController'
 const routes = Router()
 
 routes.post('/api/game/user', UserController.create)
+routes.get('/api/game/user/:username', UserController.readyByUsername)
 
 export default routes
