885b480afa261f0fdfcfe0e55e89612e7f534af2
mustaffxx <felipeamorimw@gmail.com>
2022-10-03 22:24:23 -0300

    added path param

--- src/routes/mobRoutes.ts
+++ src/routes/mobRoutes.ts
@@ -4,5 +4,6 @@ import MobController from '../controllers/MobController'
 const routes = Router()
 
 routes.post('/api/game/mob', MobController.create)
+routes.get('/api/game/mob/:name', MobController.readyByName)
 
 export default routes
