cfce65ac5295f9500a533231f583fdaff4b48f53
mustaffxx <felipeamorimw@gmail.com>
2022-10-04 21:24:29 -0300

    refactored some tests to decouple dependency of others routes

--- src/__tests__/routes.spec.ts
+++ src/__tests__/routes.spec.ts
@@ -49,7 +49,10 @@ describe('playerRoutes', () => {
         })
 
         it('should return 409 status code if player already exists', async () => {
-            await supertest(app).post('/api/game/player').send(fakePlayers[0])
+            await playerRepository.create({
+                username: fakePlayers[0].username,
+                role: fakePlayers[0].role
+            }).save()
 
             const response = await supertest(app).post('/api/game/player').send(fakePlayers[0])
 
@@ -59,7 +62,10 @@ describe('playerRoutes', () => {
 
     describe('readyByUsername', () => {
         it('should return 200 status code with player', async () => {
-            await supertest(app).post('/api/game/player').send(fakePlayers[0])
+            await playerRepository.create({
+                username: fakePlayers[0].username,
+                role: fakePlayers[0].role
+            }).save()
 
             const response = await supertest(app).get('/api/game/player/' + fakePlayers[0].username)
 
@@ -81,17 +87,18 @@ describe('playerRoutes', () => {
 
     describe('deleteByUsername', () => {
         it('should return 200 status code with empty values in properties', async () => {
-            const username = fakePlayers[0].username
-            const role = fakePlayers[0].role
-            await playerRepository.create({ username, role }).save()
+            await playerRepository.create({
+                username: fakePlayers[0].username,
+                role: fakePlayers[0].role
+            }).save()
 
-            const response = await supertest(app).delete('/api/game/player/' + username)
+            const response = await supertest(app).delete('/api/game/player/' + fakePlayers[0].username)
 
             expect(response.status).toBe(200)
 
             expect(response.body).toEqual({
-                username: username,
-                role: role,
+                username: fakePlayers[0].username,
+                role: fakePlayers[0].role,
                 experience: 0
             })
         })
@@ -227,7 +234,11 @@ describe('mobRoutes', () => {
         })
 
         it('should return 409 status code if mob already exists', async () => {
-            await supertest(app).post('/api/game/mob').send(fakeMobs[0])
+            await mobRepository.create({
+                name: fakeMobs[0].name,
+                classification: fakeMobs[0].classification,
+                experience: fakeMobs[0].experience
+            }).save()
 
             const response = await supertest(app).post('/api/game/mob').send(fakeMobs[0])
 
@@ -237,7 +248,11 @@ describe('mobRoutes', () => {
 
     describe('readyByName', () => {
         it('should return 200 status code with mob', async () => {
-            await supertest(app).post('/api/game/mob').send(fakeMobs[0])
+            await mobRepository.create({
+                name: fakeMobs[0].name,
+                classification: fakeMobs[0].classification,
+                experience: fakeMobs[0].experience
+            }).save()
 
             const response = await supertest(app).get('/api/game/mob/' + fakeMobs[0].name)
 
@@ -259,20 +274,20 @@ describe('mobRoutes', () => {
 
     describe('deleteByName', () => {
         it('should return 200 status code with empty values in properties', async () => {
-            const name = fakeMobs[0].name
-            const classification = fakeMobs[0].classification
-            const experience = fakeMobs[0].experience
+            await mobRepository.create({
+                name: fakeMobs[0].name,
+                classification: fakeMobs[0].classification,
+                experience: fakeMobs[0].experience
+            }).save()
 
-            await mobRepository.create({ name, classification, experience }).save()
-
-            const response = await supertest(app).delete('/api/game/mob/' + name)
+            const response = await supertest(app).delete('/api/game/mob/' + fakeMobs[0].name)
 
             expect(response.status).toBe(200)
 
             expect(response.body).toEqual({
-                name: name,
-                classification: classification,
-                experience: experience
+                name: fakeMobs[0].name,
+                classification: fakeMobs[0].classification,
+                experience: fakeMobs[0].experience
             })
         })
 
