21ab32df82c5dda12e8fc04a368f3190b4a25b7f
mustaffxx <felipeamorimw@gmail.com>
2022-10-05 22:59:54 -0300

    added required setup for swagger docs

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,9 @@ import express from 'express'
 
 require('express-async-errors')
 
+import YAML from 'yamljs'
+import swaggerUI from 'swagger-ui-express'
+
 import { errorHandler } from './middlewares/errorHandler'
 
 import undefinedRoute from './routes/undefinedRoute'
@@ -11,6 +14,9 @@ import mobRoutes from './routes/mobRoutes'
 const app = express()
 
 app.use(express.json())
+
+const swaggerJsDocs = YAML.load('./src/docs.yaml')
+app.use('/docs', swaggerUI.serve, swaggerUI.setup(swaggerJsDocs))
 
 /* Routes */
 app.use(playerRoutes)
