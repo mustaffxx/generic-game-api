1cc5f563e3dbdf48e2c0591ec28a2442b907cf71
mustaffxx <felipeamorimw@gmail.com>
2022-10-06 21:34:23 -0300

    removed swagger docs until project finished

--- src/app.ts
+++ src/app.ts
@@ -2,9 +2,6 @@ import express from 'express'
 
 require('express-async-errors')
 
-import YAML from 'yamljs'
-import swaggerUI from 'swagger-ui-express'
-
 import { errorHandler } from './middlewares/errorHandler'
 
 import undefinedRoute from './routes/undefinedRoute'
@@ -14,9 +11,6 @@ import mobRoutes from './routes/mobRoutes'
 const app = express()
 
 app.use(express.json())
-
-const swaggerJsDocs = YAML.load('./src/docs.yaml')
-app.use('/docs', swaggerUI.serve, swaggerUI.setup(swaggerJsDocs))
 
 /* Routes */
 app.use(playerRoutes)
